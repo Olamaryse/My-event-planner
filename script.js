d6c76cbe6f77153fd55e17a083424aeee9d98b9a
Olamaryse <114025924+Olamaryse@users.noreply.github.com>
2023-01-30 20:02:07 +0000

    Styled the table

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     // Save event when user leaves textarea
-    $('.event').blur(function() {
+    $('#task').blur(function() {
       var event = $(this).val();
       var hour = $(this).parent().attr('id');
-      localStorage.setItem(hour, event);
+      localStorage.setItem(hour, task);
     });
     
     // Load saved events on page load
     $.each(localStorage, function(key, value) {
-      $('#' + key + ' .event').val(value);
+      $('#' + key + ' #task').val(value);
     });
   });
