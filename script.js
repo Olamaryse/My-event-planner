e42081ebeb947dbab3c8deb41174e16b663e68f4
Olamaryse <114025924+Olamaryse@users.noreply.github.com>
2023-01-30 20:13:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
+    $("#add-task").click(function() {
+      var task = $("#task").val();
+      if (task) {
+        $("#tasks").append(#task);
+        $("#task").val("");
+      }
+    });
+  });
+
+$(document).ready(function() {
     // Save event when user leaves textarea
     $('#task').blur(function() {
       var event = $(this).val();
       var hour = $(this).parent().attr('id');
       localStorage.setItem(hour, task);
     });
-    
-    // Load saved events on page load
+
+//   Load saved events on page load
     $.each(localStorage, function(key, value) {
       $('#' + key + ' #task').val(value);
     });
-  });
+ 
+});
+
+
+
+
+
+
+
+    
+//     // Load saved events on page load
+//     $.each(localStorage, function(key, value) {
+//       $('#' + key + ' #task').val(value);
+//     });
+//   });
